719e46f0a53331b60ec8afa88a8f4dbca0e9e686
Mrinank Mahajan <guptamrinank@gmail.com>
2026-09-20 19:24:15 +0530

    Output the SNS topic ARN and notifications queue URL

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,5 @@ output "queue_url" { value = module.orders_queue.url }
 output "table_name" { value = module.orders_table.name }
 output "bucket_name" { value = module.receipts_bucket.name }
 output "lambda_name" { value = module.processor.name }
+output "topic_arn" { value = module.order_events.arn }
+output "notifications_queue_url" { value = aws_sqs_queue.notifications.url }
